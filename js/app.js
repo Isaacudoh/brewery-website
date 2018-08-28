ccede25ae2d9ca4b3a0e25496756a7069add54d1
gayatri P <gaya3p13@gmail.com>
2018-08-28 21:18:26 +0530

    parallax, favicon, background and fun 🎉

--- js/app.js
+++ js/app.js
@@ -1,9 +1,62 @@
-var scrollLink = $('.scroll');
-    
+let introHeight = $('.section--intro').height() + (0.4 * $(window).height())
+let scrollLink = $('.scroll');
+
 // Smooth scrolling
-scrollLink.click(function(e) {
-    e.preventDefault();
-    $('body,html').animate({
+scrollLink.click(function (e) {
+  e.preventDefault();
+  $('body,html').animate({
     scrollTop: $(this.hash).offset().top
-    }, 1000 );
+  }, 1000);
 });
+
+
+
+$(window).scroll(function () {
+  let wScroll = $(window).scrollTop()
+
+  // INTRO
+  $('.section--intro .logo').css({
+    'transform': 'translate(0px, ' + wScroll / 4 + 'px)'
+  });
+  $('.intro__text').css({
+    'transform': 'translate(0px, ' + wScroll / 6 + 'px)'
+  });
+  $('.wine').css({
+    'transform': 'translate(0px, ' + wScroll / 3 + 'px)'
+  });
+
+  $('.hero').css({
+    'transform': 'translate(0px, -' + wScroll / 100 + '%)'
+  });
+
+  // FLOATING CARDS
+  if (wScroll > $('.section--mission').offset().top - $(window).height()) {
+
+    var offset = (Math.min(0, wScroll - $('.section--mission').offset().top + $(window).height() - 350)).toFixed();
+
+    $('.section--mission .card').css({ 'transform': 'translate(' + offset + 'px, ' + Math.abs(offset * 0.2) + 'px)' });
+    $('.section--mission .card--img').css({ 'transform': 'translate(' + Math.abs(offset) + 'px, ' + Math.abs(offset * 0.2) + 'px)' });
+
+  }
+
+  if (wScroll > $('.section--taproom').offset().top - $(window).height()) {
+
+    var offset = (Math.min(0, wScroll - $('.section--taproom').offset().top + $(window).height() - 350)).toFixed();
+
+    $('.section--taproom .card').css({ 'transform': 'translate(' + offset + 'px, ' + Math.abs(offset * 0.2) + 'px)' });
+    $('.section--taproom .card--img').css({ 'transform': 'translate(' + Math.abs(offset) + 'px, ' + Math.abs(offset * 0.2) + 'px)' });
+  }
+
+  // LANDING ELEMENTS
+  if (wScroll > $('#brewery').offset().top - ($(window).height() / 1.2)) {
+
+    $('#brewery').each(function (i) {
+
+      // setTimeout(function () {
+        $('#brewery .section__image').addClass('is-showing');
+      // }, (700 * (Math.exp(i * 0.14))) - 700);
+    });
+
+  }
+
+})
