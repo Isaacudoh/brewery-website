bb76609597ab5096b2180a5520c8cdc314464ef0
gayatri P <gaya3p13@gmail.com>
2018-08-30 19:58:08 +0530

    carousel and google fonts

--- js/app.js
+++ js/app.js
@@ -9,42 +9,21 @@ scrollLink.click(function (e) {
   }, 1000);
 });
 
-
-
 $(window).scroll(function () {
   let wScroll = $(window).scrollTop()
 
-  if (wScroll > introHeight) {
-    $('nav').addClass('alone')
-  }
-  if (wScroll < introHeight) {
-    $('nav').removeClass('alone')
-  }
-
-
-
-  // INTRO
-  if (wScroll < introHeight) {
-    $('.section--intro .logo').css({
-      'transform': 'translate(0px, -' + wScroll / 4 + 'px)'
-    });
-    $('.intro__text').css({
-      'transform': 'translate(0px, -' + wScroll / 6 + 'px)'
-    });
-  }
+  // NAVIGATION
+  if (wScroll > introHeight) { $('nav').addClass('alone') }
+  if (wScroll < introHeight) { $('nav').removeClass('alone') }
 
   // FLOATING CARDS
   if (wScroll > $('.section--mission').offset().top - $(window).height()) {
     var offset = (Math.min(0, wScroll - $('.section--mission').offset().top + $(window).height() - 350)).toFixed();
-
     $('.section--mission .card').css({ 'transform': 'translate(' + offset + 'px,0)' });
-    $('.section--mission .card--img').css({ 'transform': 'translate(' + Math.abs(offset) + 'px,0)' });
   }
 
   if (wScroll > $('.section--taproom').offset().top - $(window).height()) {
     var offset = (Math.min(0, wScroll - $('.section--taproom').offset().top + $(window).height() - 350)).toFixed();
-
-    $('.section--taproom .card').css({ 'transform': 'translate(' + offset + 'px,0)' });
     $('.section--taproom .card--img').css({ 'transform': 'translate(' + Math.abs(offset) + 'px,0)' });
   }
 
@@ -57,3 +36,35 @@ $(window).scroll(function () {
   }
 
 })
+
+const left = document.getElementById('js-left')
+const right = document.getElementById('js-right')
+const list = $('.carousel__item')
+
+let translate;
+let currentItem = 2
+let item = list[currentItem];
+let caption = item.querySelector('figcaption');
+$(caption).show()
+
+const clickStuff = hand => {
+  $(caption).hide()
+
+  if (hand === 'left') {
+    currentItem = (currentItem == 0) ? 4 : (currentItem - 1);
+  } else {
+    currentItem = (currentItem == 4) ? 0 : (currentItem + 1);
+  }
+
+  item = list[currentItem]
+  caption = item.querySelector('figcaption')
+  $(caption).show()
+
+  translate = (2-currentItem)*100
+  Object.keys(list).forEach(function(key) {
+    $(list[key]).css('transform', 'translateX(' +translate+'%)')
+  })
+}
+
+$(left).click(_ => {clickStuff('left')})
+$(right).click(_ => {clickStuff('right')})
